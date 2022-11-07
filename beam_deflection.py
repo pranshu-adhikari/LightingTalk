1e6c9f51c768cf6103d7edd5954ed144aa47ce7b
RachelPaddock <112448124+RachelPaddock@users.noreply.github.com>
2022-11-06 19:15:09 -0600

    done with 11 cases
    Joe if you want me to send a pic of the specific cases I outlied I can just message me :)

--- beam_deflection.py
+++ beam_deflection.py
@@ -98,9 +98,42 @@ def BD(R,B,H,L,A,C,End_L,End_R,q,P,Mo,load_location,E):
         #can add triangular loads here if you think we need more this is already a lot of cases
 
             
-    # #now these will be the tri and roll - deflection and angle
-    # elif End_L == 'tri':
-        
+    #now these will be the tri and roll - deflection and angle
+    #due to the nature of the tri and roll ones the options I am putting are L,L/2
+    elif End_L == 'tri':
+        if q != 0:
+            if load_location == 'L': #load throughout the whole thing
+                deflection = (5*q*L**4)/(384*E*MoI)
+                angle = (q*L**3)/(24*E*MoI)
+                print('Deflection:',deflection)
+                print('Angle:',angle)
+            elif load_location == 'l/2': #is if the load is on L/2 of the beam (left side)
+                deflection = (5*q*L**4)/(768*E*MoI)
+                angle_left = ((3*q*L**3))/(128*E*MoI)
+                angle_right = (7*q*L**3)/(384*E*MoI)
+                print('Deflection:',deflection)
+                print('Angle (left):',angle_left)
+                print('Angle (right):',angle_right)
+            else:
+                print('Error-did not write location of load')
+        if P != 0:
+            if load_location == 'L/2': #one load in the middle
+                deflection = (P*L**3)/(48*E*MoI)
+                angle = (P*L**2)/(16*E*MoI)
+                print('Deflection:',deflection)
+                print('Angle:',angle)
+            else:
+                print('Error-did not write location of load')
+        if Mo != 0:
+            if load_location == 'L/2': #located in the middle
+                deflection = 0
+                angle_left = (Mo*L)/(24*E*MoI)
+                angle_right = -1*angle_left
+                print('Deflection:',deflection)
+                print('Angle (left):',angle_left)
+                print('Angle (right):',angle_right)
+            else:
+                print('Error-did not write location of load')
     
 
 
